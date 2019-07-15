4540280eb3267a56e3d684f6f98793439c920d90
TTOzzi <devttozzi@gmail.com>
2019-07-15 21:12:13 +0900

    [GUI 계산기] 완성본

--- calculator/calc-gui.js
+++ calculator/calc-gui.js
@@ -1,12 +1,90 @@
+// JS calculator
+// by TTOzzi
+// 2019.07.15
+
+// input object
+// 입력을 담당하는 객체
 var input = { 'array': [] };
 
+// 입력받은 수식을 문자열로 리턴
 input.getInput = function () {
     return this.array.join("");
 }
 
+// 입력 배열을 초기화
+input.removeAll = function (value) {
+    this.array = [];
+    this.array.push(value);
+};
+
+// 수식이 비었는지 검사
+input.isEmpty = function () {
+    return this.array.length == 0;
+};
+
+// 계산 실행 전 준비
+// getValue() 호출 전 반드시 수행
+input.prepareCalculate = function() {
+    this.array = this.array.join("").split(" ");
+};
+
+// 수식에서 값을 읽어옴
+input.getValue = function () {
+    var str = this.array.shift();
+    var n = Number(str);
+    return n;
+};
+
+// 수식에서 연산자를 읽어옴
+input.getOperator = function () {
+    var op = this.array.shift();
+    if (op === "+" || op === "-" || op === "*" || op === "/") {
+        return op;
+    } else {
+        return "$";
+    }
+};
+
+// output object
+// 출력을 담당하는 객체
 var output = {};
 output.text = document.getElementById('output');
 
+// 계산 결과 출력
+output.print = function(str) {
+    this.text.innerHTML = str;
+};
+
+// 수식 출력
+output.display = function() {
+    this.text.innerHTML = input.getInput();
+};
+
+// calculator object
+// 계산을 담당하는 객체
+var calculator = {};
+calculator.calculate = function (first, second, op) {
+    var ret;
+    switch (op) {
+        case "+":
+            ret = first + second;
+            break;
+        case "-":
+            ret = first - second;
+            break;
+        case "*":
+            ret = first * second;
+            break;
+        case "/":
+            ret = first / second;
+            break;
+        default:
+            return NaN;
+    }
+    return ret;
+};
+
+//버튼의 핸들러
 var clickNumbers = function (event) {
     var str = event.target.innerHTML;
     console.log(str);
@@ -19,91 +97,24 @@ var clickNumbers = function (event) {
         input.array.push(str);
     }
 
-    // switch (str) {
-    //     case 'BS':
-    //         break;
-    //     case '+':
-    //     case '-':
-    //     case '*':
-    //     case '/':
-    //         break;
-    //     default:
-    // }
-
-    if (input.array.length === 0) {
+    if (input.isEmpty()) {
         output.text.innerHTML = "Empty";
     } else {
-        output.text.innerHTML = input.getInput();
+        output.display();
     }
-}
+};
 
+// ' = ' 버튼의 핸들러
 var showResult = function (event) {
-    console.log("show result");
-    console.log(event.target.innerHTML);
-}
+    input.prepareCalculate();
 
+    var result = input.getValue();
+    while (!input.isEmpty()) {
+        var op = input.getOperator();
+        var second = input.getValue();
+        result = calculator.calculate(result, second, op);
+    }
+    output.print(result);
 
-
-// input.init = function(str) {
-//     this.list = str.split(" ");   
-// };
-
-// input.empty = function() {
-//     return this.list.length == 0;
-// };
-
-// input.getValue = function() {
-//     var str = this.list.shift();
-//     var n = Number(str);
-//     return n;
-// };        
-
-// input.getOperator = function() {
-//     var op = this.list.shift();
-//     if (op === "+" || op === "-" || op === "*" || op === "/") {
-//         return op;
-//     } else {
-//         return "$";
-//     }            
-// };      
-
-// calculator = {};
-// calculator.calculate = function(first, second, op) {
-//     var ret;
-//     switch(op) {
-//         case "+":
-//         ret = first + second;
-//         break;
-//         case "-":
-//         ret = first - second;
-//         break;
-//         case "*":
-//         ret = first * second;
-//         break;
-//         case "/":
-//         ret = first / second;
-//         break;  
-//         default:
-//         return NaN;         
-//     }
-//     return ret;
-// }
-
-// var output = {};
-// output.out = document.getElementById('output');
-
-// output.print = function(value) {
-// this.out.innerHTML =  "최종 결과값은 " + value + "입니다.";
-// };    
-
-// function calc() {
-//     var str = document.getElementById('input').value;
-//     input.init(str);
-//     var result = input.getValue();
-//     while (!input.empty()) {
-//         var op = input.getOperator();
-//         var second = input.getValue();
-//         result = calculator.calculate(result, second, op);
-//     }            
-//     output.print(result);
-// }
+    input.removeAll(result);
+};
